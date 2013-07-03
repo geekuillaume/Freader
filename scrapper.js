753139e6bb645c221e55fe24ef36ea7970c0038b
Guillaume Besson <guillaume@geekuillau.me>
2013-07-03 13:59:57 +0200

    Added support for Atom feeds

--- scrapper.js
+++ scrapper.js
@@ -20,28 +20,13 @@ exports.scrap = function (feed, callback) {
 
 			parseXML(body, function (err, rss) {
 				if (err)
-					return callback({message: "Not a valid RSS feed"});
-				var items = [];
+					return callback({message: "Invalid Feed"});
 
-				try {
-					for (var i = 0; i < config.maxItems && i < rss.rss.channel[0].item.length - 1; i++) {
-						items.push({
-							title: rss.rss.channel[0].item[i].title[0],
-							link: rss.rss.channel[0].item[i].guid[0]._,
-							description: rss.rss.channel[0].item[i].description[0]
-						})
-					};
-
-					var feed = {
-						name: rss.rss.channel[0].title,
-						description: rss.rss.channel[0].description,
-						link: rss.rss.channel[0].link,
-						items: items
-					};
-				}
-				catch (e) { // If not all the fiels are inside the feed
-					return callback({message: "Not a valid RSS feed"});
-				}
+				feed = parseRSS(rss);
+				if (!feed)
+					feed = parseAtom(rss);
+				if (!feed)
+					return callback({message: "Invalid Feed"});
 				callback(err, feed);
 			});
 
@@ -53,3 +38,52 @@ exports.scrap = function (feed, callback) {
 	});
 
 }
+
+
+var parseRSS = function (rss) {
+	try {
+		var items = [];
+		for (var i = 0; i < config.maxItems && i < rss.rss.channel[0].item.length - 1; i++) {
+			items.push({
+				title: rss.rss.channel[0].item[i].title[0],
+				link: rss.rss.channel[0].item[i].guid[0]._,
+				description: rss.rss.channel[0].item[i].description[0]
+			})
+		};
+
+		var feed = {
+			name: rss.rss.channel[0].title,
+			description: rss.rss.channel[0].description,
+			link: rss.rss.channel[0].link,
+			items: items
+		};
+		return feed;
+	}
+	catch (e) { // If not all the fiels are inside the feed
+		return null;
+	}
+}
+var parseAtom = function (rss) {
+	try {
+		var items = [];
+		for (var i = 0; i < config.maxItems && i < rss.feed.entry.length - 1; i++) {
+			items.push({
+				title: rss.feed.entry[i].title[0]._,
+				link: rss.feed.entry[i].link[0].$.href,
+				description: rss.feed.entry[i].content[0]._
+			})
+		};
+		var feed = {
+			name: rss.feed.title,
+			description: "No description",
+			link: rss.feed.link[0].$.href,
+			items: items
+		};
+		return feed;
+	}
+	catch (e) { // If not all the fiels are inside the feed
+		console.log(e);
+		return null;
+	}
+}
+
